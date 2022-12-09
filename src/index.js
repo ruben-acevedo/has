5bd5e37d1342005ab01eb1940248257fba6f6011
Rubén Acevedo <rubenfsolorzano@hotmail.com>
2022-12-09 00:55:25 -0300

    add has

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
-const has = args => {
-  if (!args[2]) throw new Error('error: missing arguments')
-}
+const childProcess = require('child_process')
+const process = require('process')
+const argv = process.argv
 
+const has = () => {
+  if (!argv[2]) throw new Error('error: missing arguments')
+  for (let i = 2; i < argv.length; i++) {
+    childProcess.exec(`${argv[i]} --version`, (err, stdout) => {
+      if (err) console.log(`✕ ${argv[i]}`)
+      else console.log(`✓ ${argv[i]}`)
+    })
+  }
+}
+has()
 module.exports = has
