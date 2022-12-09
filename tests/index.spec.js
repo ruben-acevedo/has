6e4f741259cde6d49cae0c2de746ec7615dd5d4d
Rubén Acevedo <rubenfsolorzano@hotmail.com>
2022-12-09 00:56:27 -0300

    add has

--- tests/index.spec.js
+++
@@ -1,8 +0,0 @@
-const has = require('../src/index')
-
-describe('Index testing', () => {
-  test('should return error if no args', () => {
-    const args = ['0', '1']
-    expect(() => has(args)).toThrowError()
-  })
-})
